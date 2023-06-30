77711c066aac47cdcbe092a048700e85751e9c08
Carlos Medeiros <carlos.medeiros@zondax.ch>
2023-06-30 08:44:42 +0100

    change type no nat64

--- src/counter_backend/src/lib.rs
+++ src/counter_backend/src/lib.rs
@@ -1,4 +1,31 @@
-#[ic_cdk_macros::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use ic_cdk_macros::*;
+
+static mut COUNTER: u64 = 0;
+
+#[init]
+fn init() {
+    unsafe {
+        COUNTER = 0;
+    }
+}
+
+#[update]
+fn increment() {
+    unsafe {
+        COUNTER += 1;
+    }
+}
+
+#[query]
+fn get() -> u64 {
+    unsafe {
+    COUNTER
+    }
+}
+
+#[update]
+fn set(input: u64) {
+    unsafe {
+        COUNTER =input;
+    }
 }
